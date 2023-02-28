b86ddf811c1d8f0ea368b92cfe019bf9a5b2a706
caasileo <isaacaasileo.com>
2023-02-28 01:35:12 -0800

    main.h

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
@@ -16,9 +15,9 @@ int main(void)
 
 	for (i = 0; i < 8; i++)
 	{
-		putchar(text[i]);
+	        _putchar(text[i]);
 	}
-	putchar('\n');
+	_putchar('\n');
 
 	return (0);
 }
