64e1e8458d5f667ffe373118e76ff5d1eeff02b7
caasileo <isaacaasileo.com>
2023-02-28 01:39:01 -0800

    fixed

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,12 +10,12 @@
 
 int main(void)
 {
-	char text[10] = "_putchar";
+	char text[10] = "Holberton";
 	int i = 0;
 
 	for (i = 0; i < 8; i++)
 	{
-	        _putchar(text[i]);
+		_putchar(text[i]);
 	}
 	_putchar('\n');
 
